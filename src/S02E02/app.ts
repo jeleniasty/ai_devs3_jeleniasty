df4ee81174ae670c4749a89734945dec277a8088
micha <michal.jeleniasty@gmail.com>
2025-05-21 16:29:28 +0200

    first try

--- src/S02E02/app.ts
+++ src/S02E02/app.ts
@@ -1,10 +1,14 @@
-import fs from 'fs';
 import path from 'path';
 import { OpenAIService } from '../services/OpenAI/OpenAIService';
+import { OpenAIModel } from '../services/OpenAI/OpenAIModel';
 
 async function main(){
     const openai = new OpenAIService();
-    const answer = openai.analyzeImage(path.join(__dirname, 'mapa.pdf'));
+    const answer = openai.analyzeImage(path.join(__dirname, 'mapa.pdf'), {
+        model: OpenAIModel.GPT4O,
+        prompt: 'What is the name of the city in the image?',
+    });
+    console.log(answer);
 }
 
 main();
