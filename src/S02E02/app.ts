dbf2744e92e23569075d2c670de338e924716bbd
micha <michal.jeleniasty@gmail.com>
2025-05-21 16:29:17 +0200

    first try

--- src/S02E02/app.ts
+++ src/S02E02/app.ts
@@ -1,5 +1,10 @@
-async function main(){
+import fs from 'fs';
+import path from 'path';
+import { OpenAIService } from '../services/OpenAI/OpenAIService';
 
+async function main(){
+    const openai = new OpenAIService();
+    const answer = openai.analyzeImage(path.join(__dirname, 'mapa.pdf'));
 }
 
 main();
